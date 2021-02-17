cbc9b1e0d137bb49d72bd6490c330bba9881ac0e
Oleksandr Tarasenko <inet.tarasenko@gmail.com>
2021-02-17 15:41:31 +0200

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
-import {UserService} from './user.service';
-import {CounterService} from './counter.service';
 
 @NgModule({
   declarations: [
